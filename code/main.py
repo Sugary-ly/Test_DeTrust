97c84098c2ec70b1e0e6a6b4667179a734f85119
Sugary-ly <993921984@qq.com>
2025-03-21 13:49:59 +0800

    commit

--- code/main.py
+++ code/main.py
@@ -9,16 +9,16 @@ if __name__ == '__main__':
     NUM_CLASSES = 2
     HIDDEN_DIM = 64
     dataset_AES = DDDataset_txt('../data/Matrix-DeTrust')
-    _, test_list, _, _ = dataset_AES.leaveone_split('AES-Detrust01')
+    _, test_list, _, _ = dataset_AES.leaveone_split('AES-DeTrust')
     test_adjacency, test_node_features, test_graph_indicator, test_node_labels = dataset_AES.__getitem__(test_list)
     test_normalize_adjacency = normalization(test_adjacency).to(DEVICE)
     test_node_features = create_sparse_one_hot(test_node_features, INPUT_DIM, DEVICE)
     test_node_labels = tensor_from_numpy(test_node_labels, DEVICE)
-    model = ModelA(INPUT_DIM, HIDDEN_DIM, NUM_CLASSES).to(DEVICE)
-    test_model = torch.load('model.pth')
-    model.eval()
+    test_model = ModelA(INPUT_DIM, HIDDEN_DIM, NUM_CLASSES).to(DEVICE)
+    test_model = torch.load('model.pth',map_location='cpu')
+    test_model.eval()
     with torch.no_grad():
-        logits = model(test_normalize_adjacency, test_node_features, None)
+        logits = test_model(test_normalize_adjacency, test_node_features, None)
         test_logits = logits
         test_acc = torch.eq(
             test_logits.max(1)[1], test_node_labels
